8e37db7ce9041df29dcf67ccadf40d81f4077a60
Mikhail Shilkov <shilkovmike@gmail.com>
2022-05-30 11:20:20 +0300

    Create project

---
+++ Cool_Timer/app/src/main/java/com/mshilkov/cool_timer/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mshilkov.cool_timer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
