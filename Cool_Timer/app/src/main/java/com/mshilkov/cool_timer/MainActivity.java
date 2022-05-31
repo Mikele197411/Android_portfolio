bdd61d04cd9244cb9b4068e121b59781f6b4cbab
Mikhail Shilkov <shilkovmike@gmail.com>
2022-05-31 10:33:54 +0300

    Added timer behavor

--- Cool_Timer/app/src/main/java/com/mshilkov/cool_timer/MainActivity.java
+++ Cool_Timer/app/src/main/java/com/mshilkov/cool_timer/MainActivity.java
@@ -2,17 +2,117 @@ package com.mshilkov.cool_timer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private SeekBar seekBar;
+    private TextView textView;
+    private Boolean isTimerOn=false;
+    private Button button;
+    private CountDownTimer countDownTimer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        seekBar=findViewById(R.id.seekBar);
+        textView=findViewById(R.id.textView);
+        button=findViewById(R.id.button);
+        seekBar.setMax(600);
+        seekBar.setProgress(60);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
+                String result=texViewLabel(progress);
+                textView.setText(result);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 
     public void onStartTimer(View view) {
+
+        if(!isTimerOn)
+        {
+            button.setText("Stop");
+            seekBar.setEnabled(false);
+            isTimerOn=true;
+            countDownTimer= new CountDownTimer(seekBar.getProgress()*1000, 1000) {
+                @Override
+                public void onTick(long l) {
+                    int progress=(int)l/1000;
+                    String result=texViewLabel(progress);
+                    textView.setText(result);
+                }
+
+                @Override
+                public void onFinish() {
+                    MediaPlayer mediaPlayer=MediaPlayer.create(getApplicationContext(),R.raw.bell_sound);
+                    mediaPlayer.start();
+                    resestTimer();
+                }
+            };
+            countDownTimer.start();
+
+
+
+        }
+        else
+        {
+            resestTimer();
+
+        }
+
+
+    }
+
+    private String texViewLabel(int progress)
+    {
+        int minutes=progress/60;
+        int seconds=progress-(minutes*60);
+        String minutesString="";
+        String secondsString="";
+        if(minutes<10)
+        {
+            minutesString="0"+minutes;
+        }
+        else
+        {
+            minutesString=String.valueOf(minutes);
+        }
+        if(seconds<10)
+        {
+            secondsString="0"+seconds;
+        }
+        else
+        {
+            secondsString=String.valueOf(seconds);
+        }
+        return (minutesString+":"+secondsString);
+    }
+
+    private void resestTimer()
+    {
+        countDownTimer.cancel();
+        button.setText("Start");
+        seekBar.setEnabled(true);
+        textView.setText("00:60");
+        seekBar.setProgress(60);
+        isTimerOn=false;
     }
 }
