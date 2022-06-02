e2dd8ec2ac9cde7f5961065ffcb1850230e3169e
Mikhail Shilkov <shilkovmike@gmail.com>
2022-06-02 11:00:40 +0300

    Finished app

--- Cool_Timer/app/src/main/java/com/mshilkov/cool_timer/MainActivity.java
+++ Cool_Timer/app/src/main/java/com/mshilkov/cool_timer/MainActivity.java
@@ -2,6 +2,8 @@ package com.mshilkov.cool_timer;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.CheckBoxPreference;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceManager;
 
 import android.content.Intent;
@@ -16,28 +18,41 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements SharedPreferences.OnSharedPreferenceChangeListener {
 
     private SeekBar seekBar;
     private TextView textView;
-    private Boolean isTimerOn=false;
+    private boolean isTimerOn;
     private Button button;
     private CountDownTimer countDownTimer;
+    private int defaultInterval;
+    SharedPreferences sharedPreferences;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        seekBar=findViewById(R.id.seekBar);
-        textView=findViewById(R.id.textView);
-        button=findViewById(R.id.button);
+
+        seekBar = findViewById(R.id.seekBar);
+        textView = findViewById(R.id.textView);
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
         seekBar.setMax(600);
-        seekBar.setProgress(60);
+        isTimerOn = false;
+        setIntervalFromSharedPreferences(sharedPreferences);
+
+        button = findViewById(R.id.button);
+
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
-                String result=texViewLabel(progress);
-                textView.setText(result);
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                long progressInMillis = progress * 1000;
+                updateTimer(progressInMillis);
+
             }
 
             @Override
@@ -50,110 +65,142 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        sharedPreferences.registerOnSharedPreferenceChangeListener(this);
     }
 
-    public void onStartTimer(View view) {
+    public void start(View view) {
 
-        if(!isTimerOn)
-        {
+        if (!isTimerOn) {
             button.setText("Stop");
             seekBar.setEnabled(false);
-            isTimerOn=true;
-            countDownTimer= new CountDownTimer(seekBar.getProgress()*1000, 1000) {
+            isTimerOn = true;
+
+            countDownTimer = new CountDownTimer(seekBar.getProgress() * 1000, 1000) {
                 @Override
-                public void onTick(long l) {
-                    int progress=(int)l/1000;
-                    String result=texViewLabel(progress);
-                    textView.setText(result);
+                public void onTick(long millisUntilFinished) {
+
+                    updateTimer(millisUntilFinished);
+
                 }
 
                 @Override
                 public void onFinish() {
-                    SharedPreferences sharedPreferences= PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-                    if(sharedPreferences.getBoolean("enable_sound", true))
-                    {
-                        MediaPlayer mediaPlayer=MediaPlayer.create(getApplicationContext(),R.raw.bell_sound);
-                        mediaPlayer.start();
-                        resestTimer();
+
+                    SharedPreferences sharedPreferences =
+                            PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+                    if (sharedPreferences.getBoolean("enable_sound", true)) {
+
+                        String melodyName = sharedPreferences.getString("timer_melody", "bell");
+                        if (melodyName.equals("bell")) {
+                            MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(),
+                                    R.raw.bell_sound);
+                            mediaPlayer.start();
+                        } else if (melodyName.equals("alarm_siren")) {
+                            MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(),
+                                    R.raw.alarm_siren_sound);
+                            mediaPlayer.start();
+                        } else if (melodyName.equals("bip")) {
+                            MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(),
+                                    R.raw.bip_sound);
+                            mediaPlayer.start();
+                        }
                     }
-                    else
-                    {
-                        resestTimer();
-                    }
+
+                    resetTimer();
+
 
                 }
             };
             countDownTimer.start();
-
-
-
+        } else {
+            resetTimer();
         }
-        else
-        {
-            resestTimer();
-
-        }
-
 
     }
 
-    private String texViewLabel(int progress)
-    {
-        int minutes=progress/60;
-        int seconds=progress-(minutes*60);
-        String minutesString="";
-        String secondsString="";
-        if(minutes<10)
-        {
-            minutesString="0"+minutes;
+    private void updateTimer(long millisUntilFinished) {
+        int minutes = (int) millisUntilFinished/1000/60;
+        int seconds = (int) millisUntilFinished/1000 - (minutes * 60);
+
+        String minutesString = "";
+        String secondsString = "";
+
+        if (minutes < 10) {
+            minutesString = "0" + minutes;
+        } else {
+            minutesString = String.valueOf(minutes);
         }
-        else
-        {
-            minutesString=String.valueOf(minutes);
+
+        if (seconds < 10) {
+            secondsString = "0" + seconds;
+        } else {
+            secondsString = String.valueOf(seconds);
         }
-        if(seconds<10)
-        {
-            secondsString="0"+seconds;
-        }
-        else
-        {
-            secondsString=String.valueOf(seconds);
-        }
-        return (minutesString+":"+secondsString);
+
+        textView.setText(minutesString + ":" + secondsString);
     }
 
-    private void resestTimer()
-    {
+    private void resetTimer() {
         countDownTimer.cancel();
         button.setText("Start");
         seekBar.setEnabled(true);
-        textView.setText("00:60");
-        seekBar.setProgress(60);
-        isTimerOn=false;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        int id=item.getItemId();
-        if(id==R.id.action_settings)
-        {
-            Intent openSettings=new Intent(this, SettingsActivity.class);
-            startActivity(openSettings);
-            return  true;
-        }
-        else if(id==R.id.action_about)
-        {
-            Intent openAbout=new Intent(this, AboutActivity.class);
-            startActivity(openAbout);
-            return  true;
-        }
-        return super.onOptionsItemSelected(item);
+        isTimerOn = false;
+        setIntervalFromSharedPreferences(sharedPreferences);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater menuInflater=getMenuInflater();
+        MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.timer_menu, menu);
         return true;
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent openSettings =
+                    new Intent(this, SettingsActivity.class);
+            startActivity(openSettings);
+            return true;
+        } else if (id == R.id.action_about) {
+            Intent openAbout =
+                    new Intent(this, AboutActivity.class);
+            startActivity(openAbout);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setIntervalFromSharedPreferences(SharedPreferences sharedPreferences) {
+        try {
+            defaultInterval = Integer.valueOf(sharedPreferences.getString("default_interval", "30"));
+        }
+        catch (Exception ex)
+        {
+            Toast.makeText(this, ex.toString(), Toast.LENGTH_LONG).show();
+        }
+
+        long defaultIntervalInMillis = defaultInterval*1000;
+
+        updateTimer(defaultIntervalInMillis);
+
+        seekBar.setProgress(defaultInterval);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if (key.equals("default_interval")) {
+            setIntervalFromSharedPreferences(sharedPreferences);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        sharedPreferences.unregisterOnSharedPreferenceChangeListener(this);
+    }
+
 }
--- Cool_Timer/app/src/main/java/com/mshilkov/cool_timer/SettingsFragment.java
+++ Cool_Timer/app/src/main/java/com/mshilkov/cool_timer/SettingsFragment.java
@@ -1,13 +1,92 @@
 package com.mshilkov.cool_timer;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
+import android.widget.Toast;
 
+import androidx.annotation.Nullable;
+import androidx.preference.CheckBoxPreference;
+import androidx.preference.EditTextPreference;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceScreen;
 
-    public class SettingsFragment extends PreferenceFragmentCompat {
+public class SettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener, Preference.OnPreferenceChangeListener {
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+    }
+
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             addPreferencesFromResource(R.xml.timer_preferences);
+
+        SharedPreferences sharedPreferences= getPreferenceScreen().getSharedPreferences();
+        PreferenceScreen preferenceScreen=getPreferenceScreen();
+        int count=preferenceScreen.getPreferenceCount();
+        for (int i=0; i<count; i++)
+        {
+            Preference preference=preferenceScreen.getPreference(i);
+            if(!(preference instanceof CheckBoxPreference))
+            {
+                String value=sharedPreferences.getString(preference.getKey(), "");
+                setPreferenceLabel(preference, value);
+            }
+        }
+        Preference preference=findPreference("default_interval");
+        preference.setOnPreferenceChangeListener(this);
+    }
+    private void setPreferenceLabel(Preference preference, String value)
+    {
+        if(preference instanceof ListPreference)
+        {
+            ListPreference listPreference=(ListPreference) preference;
+            int index =listPreference.findIndexOfValue(value);
+            if(index>=0)
+            {
+                listPreference.setSummary(listPreference.getEntries()[index]);
+            }
+        }
+        else if(preference instanceof EditTextPreference)
+        {
+            preference.setSummary(value);
+        }
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
+        Preference preference=findPreference(s);
+        if(!(preference instanceof CheckBoxPreference))
+        {
+            String value=sharedPreferences.getString(preference.getKey(), "");
+            setPreferenceLabel(preference, value);
+        }
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if(preference.getKey().equals("default_interval"))
+        {
+            String defIntervalString=(String) newValue;
+            try {
+                int defaultInterval=Integer.parseInt(defIntervalString);
+            }
+            catch (NumberFormatException ex)
+            {
+                Toast.makeText(getContext(), "Value is not correct", Toast.LENGTH_LONG).show();
+                return  false;
+
+            }
+        }
+        return true;
     }
 }
