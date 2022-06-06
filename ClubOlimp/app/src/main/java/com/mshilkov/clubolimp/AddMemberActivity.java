67251cc9627410c55e71a3be9023f979bc709256
Mikhail Shilkov <shilkovmike@gmail.com>
2022-06-06 09:27:08 +0300

    Added new activity

--- ClubOlimp/app/src/main/java/com/mshilkov/clubolimp/AddMemberActivity.java
+++ ClubOlimp/app/src/main/java/com/mshilkov/clubolimp/AddMemberActivity.java
@@ -3,12 +3,32 @@ package com.mshilkov.clubolimp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class AddMemberActivity extends AppCompatActivity {
+
+    private EditText firstNameEditText;
+    private EditText lastNameEditText;
+    private EditText groupEditText;
+    private Spinner genderSpinner;
+    private int gender=0;
+    private ArrayAdapter arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_member);
+        firstNameEditText=findViewById(R.id.firstNameEditText);
+        lastNameEditText=findViewById(R.id.lastNameEditText);
+        genderSpinner=findViewById(R.id.genderSpinner);
+        groupEditText=findViewById(R.id.groupEditText);
+
+        arrayAdapter=ArrayAdapter.createFromResource(this, R.array.array_gender, android.R.layout.simple_spinner_item);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        genderSpinner.setAdapter(arrayAdapter);
     }
 }
