8f16781e0523b4c8f23a313d1ee71012440d0fd2
Mikhail Shilkov <shilkovmike@gmail.com>
2022-06-07 10:11:14 +0300

    Added content provider

--- ClubOlimp/app/src/main/java/com/mshilkov/clubolimp/data/CLubOlympContract.java
+++ ClubOlimp/app/src/main/java/com/mshilkov/clubolimp/data/CLubOlympContract.java
@@ -1,5 +1,6 @@
 package com.mshilkov.clubolimp.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 public final class CLubOlympContract {
@@ -9,6 +10,12 @@ public final class CLubOlympContract {
     }
     public static final int DB_VERSION=0;
     public static final String DB_NAME="Olimp";
+    public static final String SCHEME = "content://";
+    public static final String AUTHORITY = "package com.mshilkov.clubolimp";
+    public static final String PATH_MEMBERS = "members";
+    public static final Uri BASE_CONTENT_URI =
+            Uri.parse(SCHEME + AUTHORITY);
+
     public static final class memberEntry implements BaseColumns
     {
         public static final String TABLE_NAME="members";
@@ -17,7 +24,8 @@ public final class CLubOlympContract {
         public static final String KEY_LASTNAME="lastname";
         public static final String KEY_GENDER="gender";
         public static final String KEY_SPORT="sport";
-
+        public static final Uri CONTENT_URI =
+                Uri.withAppendedPath(BASE_CONTENT_URI, PATH_MEMBERS);
         public static final int GENDER_UNKNOW=0;
         public static final int GENDER_MALE=1;
         public static final int GENDER_FEMALE=2;
