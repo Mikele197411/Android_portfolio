e6a7854e699a3646ea9c6bf8a50426188acf0612
Mikhail Shilkov <shilkovmike@gmail.com>
2022-06-08 15:45:20 +0300

    Added content provider

--- ClubOlimp/app/src/main/java/com/mshilkov/clubolimp/data/CLubOlympContract.java
+++ ClubOlimp/app/src/main/java/com/mshilkov/clubolimp/data/CLubOlympContract.java
@@ -1,5 +1,6 @@
 package com.mshilkov.clubolimp.data;
 
+import android.content.ContentResolver;
 import android.net.Uri;
 import android.provider.BaseColumns;
 
@@ -34,6 +35,8 @@ public final class CLubOlympContract {
 
         public static final Uri CONTENT_URI =
                 Uri.withAppendedPath(BASE_CONTENT_URI, PATH_MEMBERS);
+        public static final String CONTENT_MULTIPLE_ITEMS = ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + AUTHORITY + "/" + PATH_MEMBERS;
+        public static final String CONTENT_SINGLE_ITEM = ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + AUTHORITY + "/" + PATH_MEMBERS;
 
     }
 }
--- ClubOlimp/app/src/main/java/com/mshilkov/clubolimp/data/OlimpContentProvider.java
+++ ClubOlimp/app/src/main/java/com/mshilkov/clubolimp/data/OlimpContentProvider.java
@@ -1,10 +1,13 @@
 package com.mshilkov.clubolimp.data;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -27,30 +30,234 @@ public class OlimpContentProvider extends ContentProvider {
 
 
     @Override
-    public Cursor query( Uri uri,  String[] strings,  String s,  String[] strings1,  String s1) {
-        return null;
+    public Cursor query(Uri uri, String[] projection,
+                        String selection, String[] selectionArgs,
+                        String sortOrder) {
+        SQLiteDatabase db = olimpDBHelper.getReadableDatabase();
+        Cursor cursor;
+
+        int match = uriMatcher.match(uri);
+
+
+
+        switch (match) {
+            case MEMBERS:
+                cursor = db.query(CLubOlympContract.MemberEntry.TABLE_NAME, projection, selection,
+                        selectionArgs, null, null, sortOrder);
+                break;
+
+            // selection = "_id=?"
+            // selectionArgs = 34
+            case MEMBER_ID:
+                selection = CLubOlympContract.MemberEntry._ID + "=?";
+                selectionArgs = new String[]
+                        {String.valueOf(ContentUris.parseId(uri))};
+                cursor = db.query(CLubOlympContract.MemberEntry.TABLE_NAME, projection, selection,
+                        selectionArgs, null, null, sortOrder);
+                break;
+
+            default:
+                throw new IllegalArgumentException("Can't query incorrect URI "
+                        + uri);
+
+        }
+
+        cursor.setNotificationUri(getContext().getContentResolver(), uri);
+
+        return cursor;
     }
 
 
     @Override
-    public Uri insert(Uri uri,  ContentValues contentValues) {
-        return null;
+    public Uri insert(Uri uri, ContentValues values) {
+
+        String firstName = values.getAsString(CLubOlympContract.MemberEntry.COLUMN_FIRST_NAME);
+        if (firstName == null) {
+            throw new IllegalArgumentException("You have to input first name");
+        }
+
+        String lastName = values.getAsString(CLubOlympContract.MemberEntry.COLUMN_LAST_NAME);
+        if (lastName == null) {
+            throw new IllegalArgumentException("You have to input last name");
+        }
+
+        Integer gender = values.getAsInteger(CLubOlympContract.MemberEntry.COLUMN_GENDER);
+        if (gender == null || !(gender == CLubOlympContract.MemberEntry.GENDER_UNKNOWN || gender ==
+                CLubOlympContract.MemberEntry.GENDER_MALE || gender == CLubOlympContract.MemberEntry.GENDER_FEMALE)) {
+            throw new IllegalArgumentException
+                    ("You have to input correct gender");
+        }
+
+        String sport = values.getAsString(CLubOlympContract.MemberEntry.COLUMN_SPORT);
+        if (sport == null) {
+            throw new IllegalArgumentException("You have to input sport");
+        }
+
+        SQLiteDatabase db = olimpDBHelper.getWritableDatabase();
+
+        int match = uriMatcher.match(uri);
+
+        switch (match) {
+            case MEMBERS:
+                long id = db.insert(CLubOlympContract.MemberEntry.TABLE_NAME,
+                        null, values);
+                if (id == -1) {
+                    Log.e("insertMethod",
+                            "Insertion of data in the table failed for "
+                                    + uri);
+                    return null;
+                }
+
+                getContext().getContentResolver().notifyChange(uri,
+                        null);
+
+                return ContentUris.withAppendedId(uri, id);
+
+            default:
+                throw new IllegalArgumentException("Insertion of data in " +
+                        "the table failed for " + uri);
+
+        }
+
     }
 
     @Override
-    public int delete( Uri uri, String s,  String[] strings) {
-        return 0;
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+
+        SQLiteDatabase db = olimpDBHelper.getWritableDatabase();
+
+        int match = uriMatcher.match(uri);
+
+        int rowsDeleted;
+
+
+
+        switch (match) {
+            case MEMBERS:
+
+                rowsDeleted = db.delete(CLubOlympContract.MemberEntry.TABLE_NAME, selection,
+                        selectionArgs);
+                break;
+
+            // selection = "_id=?"
+            // selectionArgs = 34
+            case MEMBER_ID:
+                selection = CLubOlympContract.MemberEntry._ID + "=?";
+                selectionArgs = new String[]
+                        {String.valueOf(ContentUris.parseId(uri))};
+                rowsDeleted = db.delete(CLubOlympContract.MemberEntry.TABLE_NAME, selection,
+                        selectionArgs);
+                break;
+
+            default:
+                throw new IllegalArgumentException("Can't delete this URI "
+                        + uri);
+
+        }
+
+        if (rowsDeleted != 0) {
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+
+        return rowsDeleted;
     }
 
     @Override
-    public int update(Uri uri,  ContentValues contentValues,  String s,  String[] strings) {
-        return 0;
+    public int update(Uri uri, ContentValues values, String selection,
+                      String[] selectionArgs) {
+
+        if (values.containsKey(CLubOlympContract.MemberEntry.COLUMN_FIRST_NAME)) {
+            String firstName = values.getAsString(CLubOlympContract.MemberEntry.COLUMN_FIRST_NAME);
+            if (firstName == null) {
+                throw new IllegalArgumentException
+                        ("You have to input first name");
+            }
+        }
+
+        if (values.containsKey(CLubOlympContract.MemberEntry.COLUMN_LAST_NAME)) {
+            String lastName = values.getAsString(CLubOlympContract.MemberEntry.COLUMN_LAST_NAME);
+            if (lastName == null) {
+                throw new IllegalArgumentException
+                        ("You have to input last name");
+            }
+        }
+
+        if (values.containsKey(CLubOlympContract.MemberEntry.COLUMN_GENDER)) {
+            Integer gender = values.getAsInteger(CLubOlympContract.MemberEntry.COLUMN_GENDER);
+            if (gender == null || !(gender == CLubOlympContract.MemberEntry.GENDER_UNKNOWN || gender ==
+                    CLubOlympContract.MemberEntry.GENDER_MALE || gender == CLubOlympContract.MemberEntry.GENDER_FEMALE)) {
+                throw new IllegalArgumentException("You have to input correct gender");
+            }
+        }
+
+        if (values.containsKey(CLubOlympContract.MemberEntry.COLUMN_SPORT)) {
+            String sport = values.getAsString(CLubOlympContract.MemberEntry.COLUMN_SPORT);
+            if (sport == null) {
+                throw new IllegalArgumentException("You have to input sport");
+            }
+        }
+
+        SQLiteDatabase db = olimpDBHelper.getWritableDatabase();
+
+        int match = uriMatcher.match(uri);
+
+        int rowsUpdated;
+
+        switch (match) {
+            case MEMBERS:
+
+                rowsUpdated = db.update(CLubOlympContract.MemberEntry.TABLE_NAME, values,
+                        selection, selectionArgs);
+
+                break;
+            // selection = "_id=?"
+            // selectionArgs = 34
+            case MEMBER_ID:
+                selection = CLubOlympContract.MemberEntry._ID + "=?";
+                selectionArgs = new String[]
+                        {String.valueOf(ContentUris.parseId(uri))};
+                rowsUpdated = db.update(CLubOlympContract.MemberEntry.TABLE_NAME, values,
+                        selection, selectionArgs);
+
+                break;
+
+            default:
+                throw new IllegalArgumentException("Can't update this URI "
+                        + uri);
+
+        }
+
+        if (rowsUpdated != 0) {
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+
+        return rowsUpdated;
     }
 
     @Override
     public String getType(Uri uri) {
-        return null;
+
+        int match = uriMatcher.match(uri);
+
+
+
+        switch (match) {
+            case MEMBERS:
+
+                return CLubOlympContract.MemberEntry.CONTENT_MULTIPLE_ITEMS;
+
+            case MEMBER_ID:
+
+                return CLubOlympContract.MemberEntry.CONTENT_SINGLE_ITEM;
+
+            default:
+                throw new IllegalArgumentException("Unknown URI: " + uri);
+
+        }
     }
 
-//// content://com.android.mshilkov.clubolimp/members
 }
+
+
+//// content://com.android.mshilkov.clubolimp/members
+
