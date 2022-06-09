2f1227bc15abc6982cc863ba3360ecef02ea979e
Mikhail Shilkov <shilkovmike@gmail.com>
2022-06-09 17:38:46 +0300

    small fixes

--- ClubOlimp/app/src/main/java/com/mshilkov/clubolimp/MainActivity.java
+++ ClubOlimp/app/src/main/java/com/mshilkov/clubolimp/MainActivity.java
@@ -3,17 +3,23 @@ package com.mshilkov.clubolimp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.mshilkov.clubolimp.data.CLubOlympContract;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
+    MemberCursorAdapter memberCursorAdapter;
 
+    ListView dataListView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        dataListView=findViewById(R.id.dataListView);
         FloatingActionButton floatingActionButton=findViewById(R.id.floatingActionButton);
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -22,5 +28,28 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        dispalyData();
+    }
+    private void dispalyData()
+    {
+        String[] projection = {
+                CLubOlympContract.MemberEntry._ID,
+                CLubOlympContract.MemberEntry.COLUMN_FIRST_NAME,
+        CLubOlympContract.MemberEntry.COLUMN_LAST_NAME,
+                CLubOlympContract.MemberEntry.COLUMN_SPORT
+        };
+
+        Cursor cursor=getContentResolver().query(
+                CLubOlympContract.MemberEntry.CONTENT_URI,
+                projection, null, null, null
+        );
+        memberCursorAdapter=new MemberCursorAdapter(this, cursor, true);
+
+        dataListView.setAdapter(memberCursorAdapter);
     }
 }
