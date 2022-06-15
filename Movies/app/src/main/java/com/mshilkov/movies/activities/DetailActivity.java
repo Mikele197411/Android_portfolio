c5ea1dc1542f34391af376e24783b97dff572ebf
Mikhail Shilkov <shilkovmike@gmail.com>
2022-06-15 12:19:11 +0300

    small fixes

--- Movies/app/src/main/java/com/mshilkov/movies/activities/DetailActivity.java
+++ Movies/app/src/main/java/com/mshilkov/movies/activities/DetailActivity.java
@@ -2,11 +2,23 @@ package com.mshilkov.movies.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.mshilkov.movies.R;
+import com.squareup.picasso.Picasso;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -23,5 +35,9 @@ public class DetailActivity extends AppCompatActivity {
         yearTextView = findViewById(R.id.yearTextView);
         titleTextView.setText(intent.getString("title"));
         yearTextView.setText(intent.getString("year"));
+         String uri=intent.getString("posterUrl");
+        Picasso.get().load(uri).fit().centerInside()
+                .into(posterImageView);
     }
+
 }
