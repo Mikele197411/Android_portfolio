b9ee7738da5138697024883982622ac759202834
Mikhail Shilkov <shilkovmike@gmail.com>
2022-07-18 08:28:29 +0300

    Added splash screen activity

---
+++ TaxiApp/app/src/main/java/com/mshilkov/taxiapp/SplashScreenActivity.java
@@ -0,0 +1,43 @@
+package com.mshilkov.taxiapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_sceen);
+        Thread thread=new Thread(){
+            @Override
+            public void run()
+            {
+                try {
+                    sleep(5000);
+                }
+                catch (Exception ex)
+                {
+                    ex.printStackTrace();
+                }
+                finally {
+                    startActivity(new Intent(SplashScreenActivity.this, SignInActivity.class));
+
+                }
+            }
+
+
+        };
+        thread.start();
+
+
+    }
+}
